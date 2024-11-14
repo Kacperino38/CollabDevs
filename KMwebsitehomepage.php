2115ac63b0fc40d9fd0c0d47c580baa9d5fa3930
Kacper Machnicki <2342154@csl-student.wlv.ac.uk>
2024-11-14 14:20:53 +0000

    Basic homepage created

---
+++ KMwebsitehomepage.php
@@ -0,0 +1,115 @@
+<!DOCTYPE html> 
+<html lang="en"> 
+<meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <title> KMwebsite </title> 
+     
+    <style>  
+ 
+        * { 
+    padding:0; 
+    margin:0;   
+        }       
+ 
+        body{ 
+            background-color: rgb(255, 228, 181);
+
+        } 
+ 
+ 
+        .page-padding { 
+			margin-left: 12%; 
+			margin-right: 12%; 
+			padding-bottom: 80px; 
+            padding-top: 4px; 
+            padding-left: 60px; 
+            padding-right: 60px; 
+			background-color: rgb(255, 239, 213); 
+
+        } 
+
+        .space { margin-top: 100px; }
+
+        .navbar {
+            background-color: rgb(210, 170, 125) !important;
+        }
+
+        .navbar-brand {
+            color: black;
+            font-weight: bold;
+            font-size: 30px;
+        }
+
+        .navbar .btn-outline-success {
+            color: black;
+            border-color: rgb(140, 120, 90);
+        }
+
+        .navbar .btn-outline-success:hover {
+            color: black;
+            font-weight: bold;
+            border-color: black;
+            background-color: rgb(140, 120, 90);
+
+        }
+
+        .nav-link {
+            color: black;
+            font-size: 18px;
+        }
+
+        .nav-link:hover {
+            color: black;
+            font-size: 20px;
+            font-weight: bold;
+        }
+ 
+    </style> 
+     
+    <head> 
+ 
+    </head> 
+
+ 
+    <body>  
+
+    <nav class="navbar navbar-expand-lg bg-body-tertiary">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">D&D 5e Mini-Wiki</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+      <div class="navbar-nav">
+        
+        <a class="nav-link" href="KMwebsitehomepage.php">Homepage</a>
+        <a class="nav-link" href="KMwebsite1.php">Race</a>
+        <a class="nav-link" href="KMwebsite2.php">Class</a>
+        </div>
+        </div>
+        </div>
+    </nav>
+    </nav>
+
+
+        <div class = page-padding> 
+                
+        <br>
+        Hello, this is the website for Dungeons and Dragons 5e Races and Classes!
+        <br>
+        <br>
+        <br>
+        This small wiki lists all the races and classes available in the PHB for 5e Dungeons and Dragons!
+        <br>
+        Click on the links above to be directed to the table!
+        <br>
+        <br>
+        The last page is to save your own combinations to use for your own characters!
+        <br>
+<div class="space"></div>
+
+
+
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+    </body> 
