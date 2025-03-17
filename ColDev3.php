de61790f454948b014ca26852f567fd0fca7729d
uomank47 <uoman358@gmail.com>
2025-03-17 13:40:17 +0000

    fixed spelling and added info and link about travel

--- ColDev3.php
+++ ColDev3.php
@@ -118,11 +118,11 @@
 
 <div class="spaces"></div>
  
-<h1>brief history About our university </h1>
+<h1>Brief history About our university </h1>
 
 <div class="spaces"></div>
  
-<h2>early history 1827 - 19 century </h2>
+<h2>Early history 1827 - 19 century </h2>
 <h4>The University of Wolverhampton traces roots back to the 19th century, where it first started as a Mechanics Institute in 1827
   where the institute provided education and technical skills to the working class people during the industrial revolution. back then it was 
   called The Wolverhampton Mechanics Institute  and it was founded in 1827. In the latter part of the 19 century  this Institution grew even
@@ -139,7 +139,34 @@ Throughout the 70s and 80s the University or also known as the Wolverhampton Pol
   now in the present day, the university has grown into a multi-campus institution, offering education to over 21,000 students across a wide range of disciplines. The university has 3 major campuses. These campuses include City Campus located in Wolverhampton; this campus acts as a main hub of the university as majority of the subjects are taught here. The Walsall Campus, this campus Specialising in sports education, and health subjects. The Telford Campus which is known for its engineering and business programs.
   This university has come so far in its history from great challenges and struggles but as of today stands as great centre of education here in the west midlands , even today the university of Wolverhampton continues to be a leading institution in the Midlands , with very rich and diverse subjects and a passion and commitment to innovation, research.<h4>
   <br>
-  <h2> travel </h2>
+  <h2> Travel </h2>
+  <h4>
+    Traveling to the university to Wolverhampton can be expensive if u don’t live in an accommodation, 
+    but we have ways that can be cheap and well-priced, many students complain about travel to the university as they many of them 
+    live in cities like Birmingham, Coventry and Dudley . This can be challenge for those students as they must come on trains or 
+    taxi which can be very expensive in the long run.
+  </h4> <br>
+  <a href="https://ticketsearch.tfwm.org.uk/#/?allowBus&allowTrain&allowMetro&passengerType=Student&timeBand=About%20a%20month&limit=6&limitExact=6#sbmBtn">
+
+    <h2> Swift Card </h2>
+  </a>
+   <h4>
+    Currently swift are giving away student discounts for their swift cards which help students get to the university faster and cheaper 
+    than ordering a taxi or buying a rail card , we know this as students from Birmingham typically spend around £10.50 pounds per ticket
+    to the university , which can cost up to £168 per month which can be very expensive for a student but with swift it typically costs around 
+    £90 per month and you get access to not only train but you also get tram and bus with this card . we as the university of Wolverhampton 
+    recommend that all our students coming from other cities should buy a swift card as it would save your money which can be invested in better equipment for your studies .
+   </h4><br>
+
+  <h2>How To Apply</h2> 
+  <h4>
+  Apply for a swift card can generally be easy all you require is a student id and your student finance details which swift use to confirm 
+  that u are a student at the university which allows that to give you the student discount .How to cancel swift card , to cancel  the swift
+  card all you need to do is to go on the app and website to cancel the swift cards direct debit or you can do it the old fashion way and phone
+  them up , after cancelling the swift card you cannot restart the card or direct debit but instead you have to redo the whole scheme to confirm 
+  that you are a student at this university to prevent fraud .
+  </h4>
+
 
 
 
