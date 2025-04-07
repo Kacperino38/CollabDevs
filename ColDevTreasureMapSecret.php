c150a41019770f5d8c0b682074dc2c005d27fe8d
Kacper Machnicki <2342154@csl-student.wlv.ac.uk>
2025-04-07 14:06:02 +0100

    Treasure hunt page created

--- ColDevTreasureMapSecret.php
+++ ColDevTreasureMapSecret.php
@@ -3,7 +3,7 @@
 <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=0.6">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-    <title> OpenDayForm.com </title> 
+    <title> OpenDayTravel.com </title> 
     <link rel="icon" type="image/jpg" href="favicon.jpg">
      
     <style>  
@@ -19,6 +19,17 @@
       flex-direction: column;
       min-height: 100vh; /* Ensure the body takes up full height */
     } 
+
+    h2{
+      padding: 5px;
+      font-size: 25px;
+      font-weight: bold;
+    }
+
+    h4{
+      padding: 10px;
+      font-size: 20px;
+    }
  
  
         .page-padding { 
@@ -26,8 +37,8 @@
 			margin-right: 8%; 
 			padding-bottom: 80px; 
             padding-top: 4px; 
-            padding-left: 60px; 
-            padding-right: 60px; 
+            padding-left: 40px; 
+            padding-right: 40px; 
 			background-color: rgb(245, 245, 251); 
 
 
@@ -67,38 +78,45 @@
 
         .nav-link {
             color: black;
-            font-size: 20px;
+            font-size: 22px;
         }
 
         .nav-link:hover {
             color: black;
-            font-size: 20px;
+            font-size: 22px;
             font-weight: bold;
         }
- 
-        input[type="text"], input[type="number"], input[type="tel"], input[type="date"] textarea {
-            width: 100%; /* Makes them stretch to the full width of the parent container */
-            padding: 12px; /* Adds some padding for a nicer look */
-            margin-bottom: 18px; /* Adds space below each input */
-            border: 2px solid #ccc; /* Simple border around inputs */
-            box-sizing: border-box; /* Ensures padding doesn't cause overflow */
+
+        iframe
+        {
+          padding: 2px;
+          width: 85%;
+          height: 85%;
+          margin-bottom:20px;
+          border: none;
         }
 
-        form {
-            max-width: 500px; /* Max width to make the form look more compact */
-            padding: 10px;
-        }
-
-        .button {
-      background-color: rgb(162, 208, 254);
-      border-style: solid;
-      border-color: black;
-      border-width: 2px;
-      color: black;
-      font-size: 40px;
-      text-decoration: none;
-      padding: 8px 8px;
+        .contenth {
+      padding: 10px;
+      font-size: 50px;
     }
+
+
+
+    .rotate-image {
+  width: 250px; /* Optional: Set the image size */
+  height: 250px; /* Optional: Set the image size */
+  animation: rotate 5s linear infinite; /* Apply the animation */
+}
+
+@keyframes rotate {
+  from {
+    transform: rotate(0deg); /* Starting position */
+  }
+  to {
+    transform: rotate(360deg); /* Ending position */
+  }
+}
 
     </style> 
      
@@ -113,10 +131,13 @@
 
     <nav class="navbar navbar-expand-lg bg-body-tertiary">
   <div class="container-fluid">
+
     <a class="navbar-brand" href="#">OpenDayWLV!</a>
+
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
+
     <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
       <div class="navbar-nav">
         <a class="nav-link" href="ColDev1.php">Homepage</a>
@@ -125,53 +146,20 @@
         <a class="nav-link" href="ColDev6.php">Travel</a>
         </div>
         </div>
-        </div>
-                <!---Code for searchbar not yet working as no data is in place--->
-
-      
+        </div> 
     </nav>
     </nav>
 
+    <div class = page-padding> 
+    <br>    
 
+<div class="contenth"> 
+TREASURE HUNT PAGE!
+  </div>
+<div class="space"></div>
 
-        <div class = page-padding> 
-        <br>    
-        Sign up now and recieve an email about our open day!
-      <!-- UOMAN KHANS CODE -->
-		<form action="ColDev4.php" method="post">
-
-  <div class= formstyle>
-
-  <label for="first name "> First name :</label>
-  <input type="text"  id="first name " required placeholder="e.g uoman "><br>
-  <label for="last name "> Last Name:</label>
-  <input type="text" name="last name " id="last name"   required placeholder="e.g khan"><br>
-  <label for="email"> Email Address:</label>
-  <input type="text" name="email " id="email " placeholder="EmailName@gmail.com"><br>
-  <label for="phone"> Mobile Number:   </label>
-  <input type="tel" name="tel"  id="phone" placeholder="07485 123456"><br>
-  <label for="bday"> Date Of Birth:  </label>
-  <input type="date" id="bday"><br>
-  <label for="country"> Country of Origin  </label>
-  <input type="text" name="country" id="country" placeholder="United Kingdom "><br>
-  <label for="guest"> Number of Guests </label>
-  <input type="number" id="guest" min="0" max="2"><br>
-  <label for="sub"> Subjects </label>
-  <input type="text" name="sub" id="sub"  placeholder="Computer Science etc.. "><br>
- <label for="level"> Level of Study </label>
- <input type="text" name="level" id="level" placeholder="Undergraduate etc.."><br>
-Please write around 100 words, tell us about what you want to do in 
-University in terms of your study, as well as your hobbies and interests <br>
-<textarea name="comment" id="comment "></textarea>
-
-<!-- Button element -->
-<br>
-<br>
-<button type="button" class="button" onclick="alert('Thank you, Form Submitted!')">SUBMIT!</button>
-
-      </div>
-
+<marquee scrollamount="5" direction="right"><img src="pirate.jpg" class="rotate-image" alt="Rotating Image"></marquee>
 
 
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
-    </body> 
+    </body>  
