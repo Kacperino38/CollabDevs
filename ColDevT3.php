43eabbe9dcbdcfc918ad620947d6335ae505e760
uomank47 <uoman358@gmail.com>
2025-05-08 08:52:23 +0100

    hunt map page

---
+++ ColDevT3.php
@@ -0,0 +1,81 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+  <title>Treasure Hunt</title>
+  <style>
+    body {
+      background-color: #fdf5e6;
+    }
+
+    .page-padding {
+      margin-left: 12%;
+      margin-right: 12%;
+      padding: 40px 60px;
+      background-color: #fffaf0;
+      border-radius: 10px;
+    }
+
+    .navbar {
+      background-color: #8B4513 !important;
+    }
+
+    .navbar-brand, .nav-link {
+      color: gold !important;
+      font-family: 'Pirata One', cursive;
+      font-size: 24px;
+    }
+
+    h2{
+      font-family: 'Pirata One', cursive;
+      color: #8B4513;
+      margin-top: 20px;
+    }
+
+    p, ul {
+      font-size: 1.2rem;
+      line-height: 1.6;
+      color: #4B3621;
+    }
+  </style>
+  <link href="https://fonts.googleapis.com/css2?family=Pirata+One&display=swap" rel="stylesheet">
+</head>
+<body>
+  <nav class="navbar navbar-expand-lg">
+     <!--kacper add links down here !-->
+    <div class="container-fluid">
+      <a class="navbar-brand" href="#">☠️Treasure Hunt</a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNav">
+        <div class="navbar-nav">
+          <a class="nav-link" href="#">Home</a>
+          <a class="nav-link" href="#">Clues</a>
+          <a class="nav-link" href="#">Map</a>
+        </div>
+      </div>
+    </div>
+  </nav>
+
+  <div class="container">
+    <h2>Treasure Hunt Map</h2>
+    <p>Get ready for an adventure! Here's your Treasure map , your key to unlocking hidden clues scattered all across campus.
+       Every stop on this thrilling journey brings you one step closer to the legendary treasure! Let the hunt begin!</p>
+    
+    <img src="map.png.jpeg" alt="Treasure Map" class="map-img d-block mx-auto" height="800">
+
+    <h2>End of the Hunt</h2>
+<p>Thanks for taking part in the Treasure Hunt! We hope you had loads of fun exploring the campus like a true pirate. 
+   When you're done finding all the clues, go find a member of staff and say the secret phrase. If you get it right, you'll win an 
+   awesome prize of £50 credit in your WLV Wallet and a University of Wolverhampton hoodie with a cool treasure hunt design! Please 
+   remember to be respectful to other hunters and don't spoil the answer. Thanks again for joining our open day at the university of Wolverhampton, we hope to see you again soon on campus!</p>
+
+
+  
+
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
